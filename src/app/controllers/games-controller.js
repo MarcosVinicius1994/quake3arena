5977a626ec289b35b2f6d0de87b7c9db14520fce
marcos.nunes <marcosvtn@yahoo.com.br>
2022-05-27 23:31:46 -0300

    Adicionando rota de busca por ID

--- src/app/controllers/games-controller.js
+++ src/app/controllers/games-controller.js
@@ -1,4 +1,6 @@
 const { loadGamesService} = require('../services/games-service')
+const { loadGameIdService} = require('../services/games-service')
+
 
 
 module.exports = {
@@ -16,5 +18,21 @@ module.exports = {
                body: error.message
            }
         }
-    }
+    },
+    async loadGameIdController(httpRequest){
+        try {
+           const resultGame = await loadGameIdService(httpRequest)
+           return {
+               body: resultGame,
+               statusCode: 200
+           }
+        } catch (error) {
+           return {
+               statusCode: 500,
+               body: error.message
+           }
+        }
+    },
+
+
 }
--- src/app/routes/games-router.js
+++ src/app/routes/games-router.js
@@ -2,30 +2,27 @@ const { Router } = require('express')
 const { celebrate, Joi, Segments } = require('celebrate')
 const expressCallback = require('../utils/express-callback')
 const  { loadGameController } = require('../controllers/games-controller')
+const  { loadGameIdController } = require('../controllers/games-controller')
 const gamesRouter = Router()
-
-
-
 
 gamesRouter.get('/result',
 expressCallback(loadGameController))
 
 
+gamesRouter.get('/result/gameId',
+celebrate({
+    [Segments.QUERY]:{
+        idGame: Joi.number()
+        .required()
+        .messages({
+            'number.baseq3': 'Param id needs to be a number',
+            'any.required': 'Define any required',
+            'number.empty': 'Define any number',
+            'number.pattern': 'Define any number inside pattern'
+        }),
+    },
+}),
+expressCallback(loadGameIdController))
 
-// gamesRouter.get('/result/player',
-// celebrate({
-//     [Segments.QUERY]:{
-//         indexGame: Joi.number()
-//         .required()
-//         .message({
-//             'number.base': 'O campo indexGame deve um numero',
-//         }),
-//         namePlayer: Joi.string()
-//         .require()
-//         .message({
-//             'string.base': 'O campo deve ser uma palavra que representa um nome',
-//         })
-//     }
-// }))
 
 module.exports = gamesRouter
